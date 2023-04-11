d8a6270b6f8b585e6f6c984d06509f53351058bb
OksanaZabl <73551722+oksanazabl@users.noreply.github.com>
2023-04-12 01:14:30 +0300

    update App

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,40 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+import { Component } from 'react';
+import Section from './Section';
+import Counter from './Counter';
+
+class App extends Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
+
+  addFeedback = (event) => {
+    this.setState(prevState => {
+      return {
+        [event.target.name]: prevState[event.target.name] + 1,
+      };
+    });
+  };
+  
+
+  render() {
+    return (
+    <div>
+      <Section title="Please leave feedback">
+      <Counter onFeedback={this.addFeedback}/>
+    </Section>
+     <Section title="Statistics">
+     
+     </Section>
     </div>
-  );
-};
+
+    )
+
+    
+    
+ 
+  }
+}
+
+export default App;
