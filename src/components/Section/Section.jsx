20f835fc2b90b150a2e542e30b3cd2421689e835
OksanaZabl <73551722+oksanazabl@users.noreply.github.com>
2023-05-04 21:48:28 +0300

    refactoring section

--- src/components/Section/Section.jsx
+++ src/components/Section/Section.jsx
@@ -4,7 +4,7 @@ import css from './Section.module.css';
 const Section = ({ title, children }) => {
   return (
     <section className={css.section}>
-      {title}
+      <h2>{title}</h2>
       {children}
     </section>
   );
